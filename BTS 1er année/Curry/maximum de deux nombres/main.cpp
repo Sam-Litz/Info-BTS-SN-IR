4f5134565df10c03edd37db68f74bd2d0fb622d4
Samuel Litzler <samuel.litzler39@gmail.com>
2018-10-10 11:57:01 +0200

    ajout dossier lafosse et curry

---
+++ BTS 1er année/Curry/maximum de deux nombres/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>
+
+
+
+
+
+int main()
+{
+	puts("                                    *************************************");	 
+	puts("                                    *                                   *");
+	puts("                                    * Programme pour montrer le maximum *");
+	puts("                                    *        entre deux nombres         *");	
+	puts("                                    *                                   *");
+	puts("                                    *************************************");	
+	
+	//définition des variables
+	
+	int
+	
+	// Avec des if else
+	
+	if()
+	
+}
