8c5b844911ba52edac8b3e9928272539698748d6
Samuel Litzler <samuel.litzler39@gmail.com>
2018-10-12 13:33:43 +0200

    ajout modif anglais et curry exo 3

---
+++ BTS 1er année/Curry/afficher nombres pair/main.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+ int main ()
+{
+	printf("Saisi ton nombre : \n");
+	int x;
+	scanf("%d",x);
+	//if it's an odd number, then x - 1
+
+	if(x%2==1) //modulo
+	 {
+		x--;
+	 }
+	
+	/*with ternary condition
+	
+	a%2==1 ? a-- : 0;	*/
+
+
+	//while loop
+	
+	while(x != 0  && x=x-2)
+		printf("%d\n",x);
+		x=x-2;
+		
+	
+	return 0;
+}
