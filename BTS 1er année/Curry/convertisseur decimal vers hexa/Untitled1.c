92110a90ab2c0c42b07032a892db503d4bd4c000
Sam-Master <samuel.litzler39@gmail.com>
2018-10-17 11:17:12 +0200

    ajout Lord of War et code convertisseur 10 vers 16

---
+++ BTS 1er année/Curry/convertisseur decimal vers hexa/Untitled1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+ int main ()
+{
+	long int nb10;
+	int a = 1,b,reste;
+	
+	char nb16[100]
+	
+	puts("Donne ton chiffre décimal :");
+	scanf("%ld",nb10);
+	
+	
+	
+	
+	
+	
+	
+	printf("En héxadecimal %ld donne : ",nb10)
+	
+		
+
+}
+
