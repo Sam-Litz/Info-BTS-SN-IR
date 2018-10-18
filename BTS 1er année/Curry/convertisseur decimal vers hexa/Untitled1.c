a986c6001fbec1e89749eb3b6163b135dc99f8bc
Sam-Master <samuel.litzler39@gmail.com>
2018-10-18 16:02:26 +0200

    convertisseur hexa et binaire via decimal (binaire affiche pas)

--- BTS 1er année/Curry/convertisseur decimal vers hexa/Untitled1.c
+++ BTS 1er année/Curry/convertisseur decimal vers hexa/Untitled1.c
@@ -6,20 +6,36 @@
 	long int nb10;
 	int a = 1,b,reste;
 	
-	char nb16[100]
+	char nb16[100]; //tableau de autant que l'on veut
 	
-	puts("Donne ton chiffre décimal :");
-	scanf("%ld",nb10);
+	puts("Donne ton chiffre decimal :");
+	scanf("%ld",&nb10);
 	
-	
-	
-	
-	
-	
-	
-	printf("En héxadecimal %ld donne : ",nb10)
-	
+	while (nb10 !=0)
+	{
+		reste = nb10 % 16;
 		
+		if(reste < 10)
+		{
+			reste = reste + 0x30;
+		}
+		else 
+		{
+			reste = reste + 0x37;
+		}
+	
+		nb16[a++] = reste;
+	
+		nb10 = nb10 / 16;
+	}
 
+	printf("En hexadecimal %ld donne : ",nb10);
+	
+	for(b = a; b >0; b--)
+	{
+		printf("%c", nb16[b]);
+	}
+	
+	return 0;
 }
 
