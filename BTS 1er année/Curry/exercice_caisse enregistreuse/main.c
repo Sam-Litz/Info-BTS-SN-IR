ee3fbab54bd9610d223a7551dc4b93ecf83393ff
Sam-mobile <samuel.litzler39@gmail.com>
2018-10-19 16:27:07 +0200

    donner pièce

--- BTS 1er année/Curry/exercice_caisse enregistreuse/main.c
+++ BTS 1er année/Curry/exercice_caisse enregistreuse/main.c
@@ -7,7 +7,7 @@ int main()
 	// int nbArticles;
 	
 	float i =1,somme = 0, payement = 0, reste,surplus;
-	int euros2,euros1,cts50,cts20,cts10,cts5,cts2,cts1);
+	int euros2,euros1,cts50,cts20,cts10,cts5,cts2,cts1;
 	
 
 /*	
@@ -38,7 +38,7 @@ int main()
 	if (somme>payement)
 	{
 		reste = somme - payement;
-		printf("HEP HEP HEP .... il te manque %f euros ‡ donner",reste);
+		printf("HEP HEP HEP .... il te manque %f euros a donner",reste);
 	}	
 	
 	else if (payement > somme)
@@ -50,8 +50,55 @@ int main()
 		 	printf("Vous n'avez pas saisi une bonne valeur, veillez recommencer");
 		 }	
 		 
-
+   while(somme>2)
+   {
+     somme = somme - 2;
+     euros2++;
+   }
+   
+	while(somme>1)
+   {
+     somme = somme - 1;
+     euros1++;
+   }
 	
+	while(somme>0.5)
+   {
+     somme = somme - 0.5;
+     cts50++;
+   }
 	
+	while(somme>0.2)
+   {
+     somme = somme - 0.2;
+     cts20++;
+   }
+   
+   while(somme>0.1)
+   {
+     somme = somme - 0.1;
+     cts10++;
+   }
+   
+   while(somme>0.05)
+   {
+     somme = somme - 0.05;
+     cts5++;
+   }
+   
+   while(somme>0.02)
+   {
+     somme = somme - 0.02;
+     cts2++;
+   }
+   
+   while(somme>0.01)
+   {
+     somme = somme - 0.01;
+     cts1++;
+   }
+   
+   printf("Vous avez re√ßus : \n%d pieces de 2 euros\n%d pieces de 1 euros\n%d pieces de 50 centimes\n%d pieces de 20 centimes\n%d pieces de 10 centimes\n%d pieces de 5 centimes\n%d pieces de 2 centimes\n%d pieces de 1 centime",euros2,euros1,cts50,cts20,cts10,cts5,cts2,cts1);
+   
 	return 0;
 }
